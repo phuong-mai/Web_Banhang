b8430746d9622eeb9877261a34768f5c13000a6f
phuong-mai <55681843+phuong-mai@users.noreply.github.com>
2020-12-03 09:04:07 +0700

    create source

---
+++ Web_BanHang/Web_BanHang/Context/dbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using Web_BanHang.Models;
+
+namespace Web_BanHang.Context
+{
+    public class dbContext : DbContext
+    {
+        public dbContext() : base("demoASPEntities")
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Customer>().ToTable("Customers");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+
+    }
+}
---
+++ Web_BanHang/Web_BanHang/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_BanHang.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int customer_ID { get; set; }
+
+    }
+}
